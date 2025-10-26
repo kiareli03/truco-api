0c7361324abaedabf9a2f7bb313cb3d09a7b0f6a
kiareli03 <chiareli03@hotmail.com>
2025-10-25 22:22:10 -0300

    add route sse

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, MessageEvent, Res, Sse } from '@nestjs/common';
+import express from 'express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { interval, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  index(@Res() response: express.Response) {
+    response
+      .type('text/html')
+      .send(readFileSync(join(__dirname, 'index.html')).toString());
+  }
+
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return interval(1000).pipe(
+      map((_) => ({ data: { hello: 'world' } }) as MessageEvent),
+    );
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    forceCloseConnections: true,
+    cors:true,
+  });
+  app.enableShutdownHooks();
+  await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
